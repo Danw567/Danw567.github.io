18f07856942f60ca8be062cadbd877706169fe45
Danw567 <danielwiddup7@gmail.com>
2025-04-28 22:30:11 +0100

    trying a different place

--- vite.config.js
+++ vite.config.js
@@ -6,17 +6,3 @@ export default defineConfig({
   plugins: [react()],
 })
 
-const express = require('express');
-const path = require('path');
-const app = express();
-
-// Add this line to serve .jsx files as JavaScript
-app.use((req, res, next) => {
-  if (req.url.endsWith('.jsx')) {
-    res.setHeader('Content-Type', 'application/javascript');
-  }
-  next();
-});
-
-// Serve static files (assuming your client-side code is in 'public')
-app.use(express.static(path.join(__dirname, 'public')));
