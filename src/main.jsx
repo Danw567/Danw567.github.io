45e5bb42fa8c5a7235b9f0554994d3f6d993aae9
Danw567 <danielwiddup7@gmail.com>
2025-04-28 23:25:33 +0100

    what about now?

--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-const express = require('express');
-const path = require('path');
-const app = express();
-
-// Add this line to serve .jsx files as JavaScript
-app.use((req, res, next) => {
-  if (req.url.endsWith('.jsx')) {
-    res.setHeader('Content-Type', 'application/javascript');
-  }
-  next();
-});
-
-// Serve static files (assuming your client-side code is in 'public')
-app.use(express.static(path.join(__dirname, 'public')));
-
-
 createRoot(document.getElementById('root')).render(  
   <StrictMode>
     <App />
