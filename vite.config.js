eeb9ba135936ed5b469ff2b41d49f18f8fe018fb
Danw567 <danielwiddup7@gmail.com>
2025-04-28 23:04:40 +0100

    trying again

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// In your server file (e.g., server.js or app.js)
-const express = require('express');
-const path = require('path');
-const app = express();
-
-// Add this line to serve .jsx files as JavaScript
-app.use((req, res, next) => {
-  if (req.url.endsWith('.jsx')) {
-    res.setHeader('Content-Type', 'application/javascript');
-  }
-  next();
-});
-
-// Serve static files (assuming your client-side code is in 'public')
-app.use(express.static(path.join(__dirname, 'public')));
-
-// ... rest of your server code ...
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  esbuild: {
+    loader: 'jsx'
+  },
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': '.jsx',
+      },
+    },
+  },
+});
 
