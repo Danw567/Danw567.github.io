8b4504f68fb9e74bf1af8ac33a5e84cef13a0297
Danny <dwiddup@freewebstore.com>
2025-04-29 11:48:53 +0100

    some slight tweaks

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,7 +7,7 @@ function Image() {
     return(
         <div className="image-with-border">
             <div className="inner">
-                <img height="500" width="500" src="src/assets/images/me.jpg" alt="Me on a walk with a view of the surrounding scenery in the background" />
+                <img height="500" width="500" src="/images/me.jpg" alt="Me on a walk with a view of the surrounding scenery in the background" />
             </div>
         </div>
     )
@@ -22,8 +22,8 @@ function LandingSection() {
                     <h1>Daniel Widdup</h1>
                     <h2>Full-stack Developer</h2>
                     <p><strong>I craft intuitive, secure, and scalable web experiences.</strong></p>
-                    <p>With hands-on SaaS experience and a passion for user-first design, I work across JavaScript, C#, ASP.NET, AWS, and SQL to bring ideas to life through clean code and thoughtful development.</p>
-                    <p>This portfolio is built as part of my journey learning React — constantly improving, building, and pushing my skills forward.</p>
+                    <p>With hands-on SaaS experience and a passion for user-first design, I work across JavaScript, HTML, CSS, C# and ASP.NET to bring ideas to life through clean code and thoughtful development.</p>
+                    <p>This portfolio is built as part of my journey learning React. Constantly improving, building, and pushing my skills forward.</p>
                 </div>
             </div>
         </section>      
