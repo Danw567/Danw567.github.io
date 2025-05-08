5c99b16ee8f09f90322504a82aef984f1222495b
Danw567 <danielwiddup7@gmail.com>
2025-05-08 14:43:29 +0100

    work started on the projects page and some images have been added

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -3,6 +3,61 @@ import Navigation from "./components/nav/Navigation";
 import Footer from './components/nav/nav/nav_bottom';
 import { onPageLoad } from './utilites';
 
+const myProjects = [
+    {
+        name: "some url", 
+        image: "/images/project_images/dpw-portfolio-screenshot.webp", 
+        url: "youtube.com", 
+        description: "some description", 
+        techUsed: ["HTML", "CSS", "Javascript", "React", "Typescript"]
+    },
+    {
+        name: "some url", 
+        image: "/images/project_images/crafty-base-design.webp", 
+        url: "youtube.com", 
+        description: "some description", 
+        techUsed: ["HTML", "CSS", "Javascript", "React", "Typescript"]
+    },
+    {
+        name: "some url", 
+        image: "/images/project_images/crafty-design-defrag.webp", 
+        url: "youtube.com", 
+        description: "some description", 
+        techUsed: ["HTML", "CSS", "Javascript", "React", "Typescript"]
+    }
+]
+
+function TechStackPill({techName}) {
+    return(
+        <a className='' title={`filter by projects that use ${techName}`}>{techName}</a>
+    )
+}
+
+function MyProjects() {
+    return(
+        <>
+            {myProjects.map((proj, i) => (
+                <section key={i}>
+                    <div className="project-section">
+                        <div className='project-section-image'>
+                            <img src={proj.image} alt={`screenshot of ${proj.name}`} />
+                        </div>
+                        <div className='project-section-info'>
+                            <h2>{proj.name}</h2>
+                            <div className='stack-usage'>
+                                {proj.techUsed.map(tech => (
+                                    <TechStackPill techName={tech} />
+                                ))}
+                            </div>
+                            <p>{proj.description}</p>
+                        </div>  
+                    </div>
+                </section>
+            ))}
+        </>
+    );
+}
+
 function ProjectsPage() {
     useEffect(() => {
         if (document.readyState === 'complete') {
@@ -17,8 +72,12 @@ function ProjectsPage() {
         <>
             <Navigation activePage="Projects" />
             <main>
-                <h1>ProjectsPage</h1>
-                <h2>Coming soon</h2>
+                <section>
+                    <h1>My Projects</h1>
+                    <p>This section of my portfolio lists the things I have worked on, both personally and professionally.</p>
+                </section>
+                <MyProjects />
+                
             </main>
             <Footer />
         </>
